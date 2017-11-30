d13ed3cce52856982bf26fb5dcea6eac42f5612e
LAPTOP-HINUMAAJ\Loki <thiagokoster@gmail.com>
2017-11-30 21:22:33 -0200

    Progresso no relatório

--- Trabalho2.m
+++ Trabalho2.m
@@ -1,12 +1,12 @@
 clear
 
-[y,Fs] = audioread('ch.wav');
+[y,Fs] = audioread('ss.wav');
 fn = 11025;                         %Vamos fazer um downsampling de 11025Hz
 [P,Q] = rat(fn/Fs);
 y_r = resample(y,P,Q);
 
-sample = 0.02 * fn;                %Começar a amostra de 20 ms
-y_r = y_r(sample:sample+882,:);    % Utilizar uma amostra de 80ms do sinal que sofreu downsampling
+%sample = 0.02 * fn;                %Começar a amostra de 20 ms
+%y_r = y_r(sample:end,:);    % Utilizar uma amostra de 80ms do sinal que sofreu downsampling
 
 
 p=12; % Ordem
@@ -15,12 +15,15 @@ tamanho = length(y_r);
 
 [a,varu] = Periodogram(y_r,p,'Sinal original amostrado');
 
-u = normrnd(0,sqrt(varu),tamanho,1);                       %ruido branco u(n) e variância varu
+u = normrnd(0,sqrt(varu),tamanho,1);                    %ruido branco u(n) e variância varu
 
 x = filter(1,[1 -a'],u);                                %gerar o x(n)
 
-audiowrite('Sintetizado.wav',100*x,11025);
-audiowrite('Original.wav',100*y_r,11025);
+
+
+
+audiowrite('Sintetizado.wav',x,11025);
+audiowrite('Original.wav',y_r,11025);
                                       
 % for n = p+1:tamanho                             %Sem o somatório fica
 %                                                 %bem mais parecido...
